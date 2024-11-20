5a061c5371754eea6a900684fc1e7d249b3c60f0
Madhusudan_chauhan <madhusudanchauhan62@gmail.com>
2024-11-21 00:56:30 +0530

    user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,8 @@
 import { asyncHandler } from "../utils/asycHandler.js";
 
 const registerUser = asyncHandler( async ( req, res)=>{
-    res.status(200).json({
-        message: "ok"
+    res.status(400).json({
+        message: "Madhusudan Singh chauhan "
     })
 })
 
