955ca6d6d4b196fd67e682955b04976ad0d013b7
Madhusudan_chauhan <madhusudanchauhan62@gmail.com>
2024-11-21 00:34:29 +0530

    routes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from "express"
 import cors from "cors"
 import cookieParser  from "cookie-parser"
+import userRouter from "./routes/user.routes.js"
+
 const app =  express()
 
 app.use(cors({
-    origin : ProcessingInstruction.env.CORS_ORIGIN , 
+    origin : process.env.CORS_ORIGIN, 
     Credential: true 
 }))
 app.use(express.json({limit : "16kb"}))
@@ -16,7 +18,6 @@ app.use(cookieParser())
 
 //routes import 
 
-import userRouter from './routes/user.routes.js'
 
 //routes declaration
 app.use("/api/v1/users" , userRouter)
