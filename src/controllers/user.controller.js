ca11813aded751f68f32e9f1f5ab55a89cde17cf
Madhusudan_chauhan <madhusudanchauhan62@gmail.com>
2024-11-22 01:27:07 +0530

    multer middleware

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,11 +1,25 @@
 import { asyncHandler } from "../utils/asycHandler.js";
 
 const registerUser = asyncHandler( async ( req, res)=>{
-    res.status(400).json({
-        message: "Madhusudan Singh chauhan "
-    })
+    //get a details from frontend
+    //validations- not empty
+    //check if user already exist or not 
+    //check from username , email
+     //upload image on cloudinary
+     //Create user Object - create entry in db
+     //remove password and refresh token field from response
+     //check for user creation 
+     //return response 
+     //return res
+    const{fullname , email , username , password } = req.body
+    console.log("email: " , email );
+    
+
+
+
 })
+
 
 export {
     registerUser,
-};
+}
