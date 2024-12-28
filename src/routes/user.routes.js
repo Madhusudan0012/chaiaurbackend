3ffd4dd8dde577d0d75d6c97b68ea2b3ab7b28e3
Madhusudan <madhusudanchauhan62@gmail.com>
2024-12-27 20:04:11 -0800

    commit

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -19,10 +19,11 @@ router.route("/register").post(
     ]), 
     registerUser
 )
-router.route("/login").post(loginUser)
-//secure routes 
+// )
+// router.route("/login").post(loginUser)
+// //secure routes 
 
-router.route("/logout").post(verifyJWT , logoutUser)
+// router.route("/logout").post(verifyJWT , logoutUser)
 
 
 export default router
